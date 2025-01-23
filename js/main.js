cec1ff2abf7bb9d136ca52b2ed6f0444f63be706
dasq <darifarid@gmail.com>
2025-01-23 14:48:52 +0100

    custom update

--- js/main.js
+++ js/main.js
@@ -106,6 +106,24 @@
         $('.video-mo-01').children('iframe')[0].src = srcOld;
         $('.video-mo-01').css('opacity','0');
     });
+	
+	 /*[ Play video 02]
+    ===========================================================*/
+    var srcOld = $('.video-mo-02').children('iframe').attr('src');
+
+    $('[data-target="#modal-video-02"]').on('click',function(){
+        $('.video-mo-02').children('iframe')[0].src += "&autoplay=1";
+
+        setTimeout(function(){
+            $('.video-mo-02').css('opacity','1');
+        },300);      
+    });
+
+    $('[data-dismiss="modal"]').on('click',function(){
+        $('.video-mo-02').children('iframe')[0].src = srcOld;
+        $('.video-mo-02').css('opacity','0');
+    });
+    
     
 
     /*[ Fixed Header ]
